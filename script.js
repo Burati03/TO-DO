83875ed13e94980a009a7f9869b84285a0d46333
Burati03 <gabrielhenrique@colegiopaulodetarso.com.br>
2026-03-31 14:45:26 +0000

    vfbfb

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-    const taskBtn = document.querySelector("#taskBtn") 
-    const taskList = document.querySelector("#taskList") 
-    const taskInput = document.querySelector("#taskInput") 
+const taskBtn = document.querySelector("#taskBtn")
+const taskList = document.querySelector("#taskList")
+const taskInput = document.querySelector("#taskInput")
 
-    taskBtn.addEventListener("click", function(){
-        alert("CLIQUE !!!")
+function createSpan(text){
+    const span = document.createElement('span');
+    span.textContent = text;
+    span.addEventListener('click',function(){
+        span.classList.toggle('completed')
     })
+    return span;
+}
+
+function createDeleteButton(li){
+    const button = document.createElement('button');
+    button.textContent = 'Excluir';
+
+    button.addEventListener('click', function(){
+        li.remove();
+    });
+
+    return button;    
+}
+
+function addTask(){
+    const taskInputValue = taskInput.value;
+
+    if (taskInputValue === '') return;
+
+    const li = document.createElement('li');
+
+    const span = createSpan(taskInputValue);
+    const button = createDeleteButton(li);
+
+    li.appendChild(span);
+    li.appendChild(button);
+
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+}
+
+taskBtn.addEventListener("click", addTask);
